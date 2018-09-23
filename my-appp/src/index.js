ddf1a29933c843e7d4514f5073ebff3cd415fb15
Wangmumu1009 <18846128334@163.com>
2018-09-23 14:06:31 +0800

    722

---
+++ my-appp/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import lunbo from './components/lunbo';
+
+ReactDOM.render(
+    //<Provider store={store}>
+      //<App />
+    //</Provider>
+   <lunbo></lunbo>
+    ,
+    document.getElementById('root'));
+
