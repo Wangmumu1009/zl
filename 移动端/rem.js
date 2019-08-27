7481352ca4b76d1fabfb6b0978768dadfa6ddcfe
Wangmumu1009 <18846128334@163.com>
2019-08-27 20:15:40 +0800

    ss

--- 移动端/rem.js
+++ 移动端/rem.js
@@ -7,6 +7,7 @@
         var width = docEl.getBoundingClientRect().width;
         var rem = width / 6.4; // 将屏幕宽度分成6.4份， 1份为1rem
         docEl.style.fontSize = rem + 'px';
+        //rem
         // console.log(0.14*rem);
     }
 
