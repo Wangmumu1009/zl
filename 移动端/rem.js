8c104fc864d84d5acd60fe5b579000ac9583fd12
Wangmumu1009 <18846128334@163.com>
2019-08-27 20:22:23 +0800

    ce

--- 移动端/rem.js
+++ 移动端/rem.js
@@ -4,10 +4,14 @@
     var tid;
 
     function refreshRem() {
+        alert(aaaa)
         var width = docEl.getBoundingClientRect().width;
         var rem = width / 6.4; // 将屏幕宽度分成6.4份， 1份为1rem
         docEl.style.fontSize = rem + 'px';
         //remrrr
+
+
+
         // console.log(0.14*rem);
     }
 
